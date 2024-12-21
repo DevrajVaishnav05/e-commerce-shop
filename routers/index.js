9f71b01773708a7021d57ef85a1cdbe54dc661fd
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-21 17:21:40 +0530

    create cart view and route

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import auth from "../middlewares/authLogin.js";
 import productModel from "../models/product-model.js"
+import userModel from "../models/user-model.js"
 const router = express.Router();
 router.get('/', (req, res) => {
     let error  = req.flash("error");
@@ -8,6 +9,19 @@ router.get('/', (req, res) => {
 });
 router.get('/shop', auth, async (req, res) => {
     let products = await productModel.find()
-    res.render("shop",{products});
+    let success  = req.flash("success");
+    res.render("shop",{products,success});
+});
+router.get('/cart', auth, async (req, res) => {
+    let user = await userModel.findOne({email:req.user.email}).populate("cart");
+    
+    res.render("cart",{user});
+});
+router.get('/addtocard/:productid', auth, async (req, res) => {
+    let user = await userModel.findOne({email:req.user.email});
+    user.cart.push(req.params.productid);
+      await user.save();
+      req.flash("success","Added to cart");
+      res.redirect("/shop");
 });
 export default router;
